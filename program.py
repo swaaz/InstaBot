64353cf1ab79f932140e4dfa55abe734ed942aa7
swaaz <swaasthik.shetty07@gmail.com>
2020-05-06 14:34:41 +0530

    testing is passed

--- program.py
+++ program.py
@@ -1,27 +1,32 @@
+#importing libraries and files
 from selenium import webdriver
 from time import sleep
+import sys
 import secrets
+#getting values from secrets.py file and assigning the values to username and password
 username = secrets.usr
 password = secrets.pwd
+#creating class to automate
 class Instabot():
     def __init__(self,username,password):
-        self.username = username
-        self.password = password
-        self.driver = webdriver.Firefox(executable_path = "/home/swaaz/swaaz/InstaBot/geckodriver-v0.26.0-linux64/geckodriver")
-        self.driver.get("https://instagram.com/")
+        self.username = username    #storing the username in class
+        self.password = password    #storig the password in class
+        self.driver = webdriver.Firefox(executable_path = "./geckodriver-v0.26.0-linux64/geckodriver") #this is the path of webdriver, here I used geckodriver since I am using Firefox. Change the path of webdriver if you are not using Firefox
+        self.driver.get("https://instagram.com/") 
         sleep(4)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
-        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)    #entering the username 
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)    #entering the password
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()   #clicking on login button
         sleep(6)
-        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
-        sleep(2)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()  #clicking on 'Not Now' option
+        sleep(4)
+
     def get_pending_request(self):
         self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username)).click()
         sleep(2)
         self.driver.find_element_by_xpath("//button[@type = 'button']").click()
         sleep(2)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/ul/li[7]/a").click()
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Privacy and Security')]").click()
         sleep(2)
         self.driver.find_element_by_xpath("//a[contains(text(), 'View Account Data')]").click()
         sleep(2)
@@ -39,24 +44,14 @@ class Instabot():
                 self.driver.find_element_by_xpath("//button[contains(text(), 'Requested')]").click()
                 sleep(3)
                 self.driver.find_element_by_xpath("//button[contains(text(), 'Unfollow')]").click()
-                sleep(2)
-                '''
-                self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username)).click()
                 sleep(3)
-                self.driver.find_element_by_xpath("//button[@type = 'button']").click()
-                sleep(2)
-                self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/ul/li[7]/a").click()
-                sleep(2)
-                self.driver.find_element_by_xpath("//a[contains(text(), 'View Account Data')]").click()
-                sleep(2)
-                self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/main/div/div[2]/section[1]/section[1]/a").click()
-                sleep(2)
-                '''
                 self.driver.back()
-                sleep(2)
+                sleep(3)
                 self.driver.refresh()
+                sleep(3)
             except:
-                pass
+                print("Tadaaaa!!!!!")
+                sys.exit()
         
 
     def _getnames(self):
@@ -64,7 +59,7 @@ class Instabot():
         links = scroll_box.find_element_by_class_name("-utLf").text
         return links
         
-    
+
 
 bot = Instabot(username,password)
 bot.get_pending_request()
