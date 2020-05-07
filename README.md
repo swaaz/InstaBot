04816970201a12a557c34b523deab2e989bb106d
swaaz <swaasthik.shetty07@gmail.com>
2020-05-07 21:52:58 +0530

    updated

--- README.md
+++ README.md
@@ -4,3 +4,52 @@
 <div align="center">
 <img src="./src/bot.gif" width="300x" height="400px">
 </div>
+
+### Clone project
+first you need to fork and then copy the url from clone option
+```
+$ git clone [url]
+```
+### Install Python 3
+```
+$ sudo apt-get update
+$ sudo apt-get install python3.7
+```
+### Install Selenium
+```
+$ pip3 install selenium
+```
+### Change the webdriver path
+#### the path of the webdriver has to changed according to your environment
+edit the 18ₜₕ line of file  ``` ./program.py ```
+```
+self.driver = webdriver.Firefox(executable_path = "[path]")
+```
+example:
+```
+self.driver = webdriver.Firefox(executable_path = "./webdrivers/firefox_webdriver/geckodriver-v0.26.0-linux64/geckodriver")
+```
+#### file structure:
+```
+.
+├── webdriver
+│   ├── chrome_webdriver
+│   │   ├── Chrome_webdriver_linux64
+│   │   ├── Chrome_webdriver_mac64
+│   │   ├── Chrome_webdriver_win32
+|   |
+|   ├── firefox_webdriver
+|   |   ├── geckodriver-v0.26.0-linux64
+│   │   ├── geckodriver-v0.26.0-macos
+│   │   ├── geckodriver-v0.26.0-win32
+```
+### Add username and password
+edit the ```./secrets.py``` file and add your username and passport
+```
+usr = "[username]" 
+pwd = "[password]"
+```
+### Run the program
+```
+$ pip3 program.py
+```
--- program.py
+++ program.py
@@ -3,26 +3,32 @@ from selenium import webdriver
 from time import sleep
 import sys
 import secrets
-#getting values from secrets.py file and assigning the values to username and password
+
+#getting values usr and pwd from secrets.py file and assigning the values to username and password
 username = secrets.usr
 password = secrets.pwd
+
 #creating class to automate
 class Instabot():
+
     #function to login
     def __init__(self,username,password):
         self.username = username    #storing the username in class
         self.password = password    #storig the password in class
-        self.driver = webdriver.Firefox(executable_path = "./webdrivers/firefox_webdriver/geckodriver-v0.26.0-linux64/geckodriver") #this is the path of webdriver, here I used geckodriver since I am using Firefox. Change the path of webdriver if you are not using Firefox
+        self.driver = webdriver.Firefox(executable_path = "./webdrivers/firefox_webdriver/geckodriver-v0.26.0-linux64/geckodriver") #this is the path of webdriver, here I used geckodriver-linux since I am using Firefox in linux. Change the path of webdriver according to your environment
         self.driver.get("https://instagram.com/") 
         sleep(4)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)    #entering the username 
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)    #entering the password
-        self.driver.find_element_by_xpath('//button[@type="submit"]').click()   #clicking on login button
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)    
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)    
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()   
         sleep(6)
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()  #clicking on 'Not Now' option
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()  
         sleep(4)
+
     #function to cancel all the pending follow requests
     def cancel_sent_requests(self):
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a").click()
+        sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username)).click()
         sleep(2)
         self.driver.find_element_by_xpath("//button[@type = 'button']").click()
@@ -33,6 +39,7 @@ class Instabot():
         sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/main/div/div[2]/section[1]/section[1]/a").click()
         sleep(2)
+        z = True
         while True:
             try:
                 pending_id = self._get_pending_names()
@@ -52,14 +59,18 @@ class Instabot():
                 sleep(3)
             except:
                 print("Tadaaaa!!!!!, Task Completed!!!!")
-                sys.exit()
-
+                z = False
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a").click()
+    #function which return the name from the list
     def _get_pending_names(self):
         scroll_box = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/main")
         links = scroll_box.find_element_by_class_name("-utLf").text
         return links
-    
+
+    #function to get the names of unfollowers
     def get_unfollowers(self):
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a").click()
+        sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username)).click()
         sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href,'/following')]").click()
@@ -67,10 +78,26 @@ class Instabot():
         self.driver.find_element_by_xpath("//a[contains(@href,'/followers')]").click()
         followers = self._get_names()
         not_following_back = [user for user in following if user not in followers]
-        print("****** Unfollowers ******\n")
+        print("\n****** Unfollowers ******\n")
         for x in not_following_back :
             print(x)
+        
+    #function to get the names of fans
+    def get_fans(self):
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a").click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username)).click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/following')]").click()
+        following = self._get_names()
+        self.driver.find_element_by_xpath("//a[contains(@href,'/followers')]").click()
+        followers = self._get_names()
+        fans = [user for user in followers if user not in following]
+        print("\n****** Fans ******\n")
+        for x in fans :
+            print(x)
 
+    #function which returns list of names
     def _get_names(self):
         scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
         last_ht, ht = 0, 1
@@ -80,12 +107,27 @@ class Instabot():
             ht = self.driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight); return arguments[0].scrollHeight;", scroll_box)
         links = scroll_box.find_elements_by_tag_name('a')
         names = [name.text for name in links if name.text != '']
-        # close button
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
         return names
+
+#main function
 def main():
     bot = Instabot(username,password)
-    #bot.cancel_sent_requests()
-    bot.get_unfollowers()
+    a = True
+    while a:
+        print()
+        print("****** Insta-Bot ******")
+        x = int(input("1. List the names of Un-followers\n2. Fans\n3. Cancel all the sent follow requests\n4. Exit\nEnter the choice : "))
+        if x == 1:
+            bot.get_unfollowers()
+        elif x == 2:
+            bot.get_fans()
+        elif x == 3:
+            bot.cancel_sent_requests()
+        elif x == 4:
+            a = False
+        else:
+            print("Invalid option!!")
+        
 
 main()
